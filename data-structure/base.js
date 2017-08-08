1773d2ad8faadc29f7a88aab4e344d29e4319c29
Yann001 <1156203992@qq.com>
2017-08-09 00:44:15 +0800

    add data structure-queue

--- data-structure/base.js
+++ data-structure/base.js
@@ -4,27 +4,64 @@ define(function () {
     this.top = 0;
     this.stack = [];
   }
-  Stack.prototype.empty = function () {
-    if(this.top == 0) {
-      return true;
-    } else {
-      return false;
+  Stack.prototype = {
+    isEmpty: function () {
+      if (this.top === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    push: function (item) {
+      this.stack[this.top++] = item;
+    },
+    pop: function () {
+      if (this.isEmpty()) {
+        return new Error('stack underflow');
+      } else {
+        return this.stack[this.top--]
+      }
     }
   }
-  Stack.prototype.push = function (item) {
-    this.top++;
-    this.stack[this.top] = item;
+  // 队列
+  function Queue() {
+    this.head = 0;
+    this.tail = 0;
+    this.queue = [];
   }
-  Stack.prototype.pop = function () {
-    if (this.empty()) {
-      throw Error('stack underflow');
-    } else {
-      this.top--;
-      return this.stack[this.top + 1]
+  Queue.prototype = {
+    isEmpty: function () {
+      if (this.head === this.tail) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    enqueue: function (item) {
+      if (this.queue.length === this.tail) {
+        this.tail = 0;
+        this.queue[this.tail] = item;
+      } else {
+        this.queue[this.tail++] = item;
+      }
+    },
+    dequeue: function () {
+      if (this.isEmpty()) {
+        return new Error('queue underflow');
+      } else {
+        var x = this.queue[this.head];
+        if (this.queue.length === this.head) {
+          this.head = 0;
+        } else {
+          this.head++
+        }
+        return x;
+      }
     }
   }
 
   return {
     Stack,
+    Queue,
   };
 })
